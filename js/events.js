d7deb6247b142e847dc288ef198274bd327202ff
James Jensen <jnjensen@live.com>
2020-02-21 03:39:33 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,12 @@
 //define functions here
-
-$(document).ready(function(){
-
 function getIt (){
   $('p').on('click', function (){
     alert('Hey!')
   })
 }
+
+$(document).ready(function(){
+
+getIt();
 
 });
