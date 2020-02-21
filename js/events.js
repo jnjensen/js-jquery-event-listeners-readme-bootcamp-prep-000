bc0604920bdaaf229f951bdaa9fac5c11dbe4c26
James Jensen <jnjensen@live.com>
2020-02-21 03:49:53 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -7,7 +7,7 @@ function getIt (){
 
 function frameIt(){
   $('.tasty').on('load', function (){
-    $('img').style.border = "thick, solid, red"
+    $('img').style.border = "thick solid red"
   })
 }
 
